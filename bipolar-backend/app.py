d5af7f3e7715697afb86752b09e5aa5932f7469c
XxBHAVYAxX <bhavyakanojia123@gmail.com>
2026-03-03 23:58:21 +0530

    Update app.py

--- bipolar-backend/app.py
+++ bipolar-backend/app.py
@@ -1,3 +1,12 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+CORS(app)
+
+
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.json
